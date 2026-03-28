554be34f69972c8f5d6b3ce19abeace81bfe7ee8
Ashishoct35 <ashishoct35@gmail.com>
2026-03-28 09:03:25 +0545

    Mobile redesign: 2-col KPI tiles, recs first, SW v3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'di-tracker-v2';
+const CACHE = 'di-tracker-v3';
 const SHELL = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
